e27818e353bfac300030b9496ce01f80c2b4b6e9
HARISH-SATHISH <gowdaharish580@gmail.com>
2023-12-10 18:34:34 +0530

    fixed scrollbar issue

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div style={{ position: 'relative', height: '100vh' }} >
       <VideoBg />
-      <Navbar/> 
+      <Navbar /> 
       <Homecon/>
       {/* <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
         <button className="border border-white p-2 px-4 text-white hover:bg-white hover:text-black transition-colors">
