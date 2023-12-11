464b0b1565a7a6d816dcbc8a5649220c91cb7fdf
HARISH-SATHISH <gowdaharish580@gmail.com>
2023-12-11 17:36:11 +0530

    bg mp4 to gif

--- app/components/VideoBg.tsx
+++ app/components/VideoBg.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Image from 'next/image';
+import spacegf from '../../public/spacegf.gif'
 
 const VideoBackground: React.FC = () => {
   return (
     <div className=" -z-10 top-0 left-0 w-full h-full overflow-hidden fixed">
-      <video src={require('../../public/space.mp4')}   className="w-full h-full object-cover " autoPlay muted loop/>
+    <Image src={spacegf}className="w-full h-full object-cover " alt='bg'></Image>
     </div>
   );
 };
