582c1a48ef8b58cc214cbc91953bf5f72ea1480c
HARISH-SATHISH <gowdaharish580@gmail.com>
2023-12-12 22:21:09 +0530

    log form added

---
+++ app/api/logs/logform/page.tsx
@@ -0,0 +1,131 @@
+"use client"
+import VideoBackground from '@/app/components/VideoBg';
+import React, { useState } from 'react';
+import "../../../../app/globals.css"
+
+const page = () => {
+
+  const celestialObjectOptions = ['Star', 'Planet', 'Galaxy', 'Nebula', 'Cluster', 'Unknown'];
+
+  const [formData, setFormData] = useState({
+    celestialObject: celestialObjectOptions[0], // Default to the first option
+    dateAndTime: '',
+    deviceUsed: '',
+    observationNotes: '',
+    media: null,
+  });
+
+  // Handler for form input changes
+  const handleChange = (e: any) => {
+    const { name, value, files } = e.target;
+
+    // Update state based on input type
+    if (name === 'media') {
+      setFormData({ ...formData, [name]: files[0] });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  // Handler for form submission
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    // Add logic here to handle the form submission (e.g., send data to a backend server)
+    console.log('Form Submitted:', formData);
+  };
+
+  return (
+    <div className=" mx-[10%] mt-4">
+      <VideoBackground />
+      <form onSubmit={handleSubmit} className="p-3 border text-white border-gray-300 bg-transparent ">
+        <h2 className="text-2xl font-semibold mb-4">Astronomy Observation Form</h2>
+
+        {/* Celestial Object Dropdown */}
+        <div className="mb-4">
+          <label htmlFor="celestialObject" className="block text-sm font-medium">Celestial Object</label>
+          <select
+            id="celestialObject"
+            name="celestialObject"
+            value={formData.celestialObject}
+            onChange={handleChange}
+            className="mt-1 p-2 w-full border border-gray-300  focus:outline-none focus:ring focus:border-white bg-transparent"
+            required
+          >
+            {celestialObjectOptions.map((option) => (
+              <option key={option} value={option} className='bg-black'>{option}</option>
+            ))}
+          </select>
+        </div>
+
+        {/* Date and Time */}
+        <div className="mb-4">
+          <label htmlFor="dateAndTime" className="block text-sm font-medium">Date and Time</label>
+          <input
+            type="datetime-local"
+            id="dateAndTime"
+            name="dateAndTime"
+            value={formData.dateAndTime}
+            onChange={handleChange}
+            className="mt-1 p-2 w-full border border-gray-300  focus:outline-none focus:ring focus:border-white bg-transparent" // Updated styling here
+            required
+          />
+        </div>
+
+        {/* Device Used (formerly Telescope Used) */}
+        <div className="mb-4">
+          <label htmlFor="deviceUsed" className="block text-sm font-medium">Device Used</label>
+          <input
+            type="text"
+            id="deviceUsed"
+            name="deviceUsed"
+            value={formData.deviceUsed}
+            onChange={handleChange}
+            placeholder='deviceused'
+            className="mt-1 p-2 w-full border border-gray-300  focus:outline-none focus:ring focus:border-white bg-transparent text-white"
+            required
+          />
+        </div>
+
+        {/* Observation Notes */}
+        <div className="mb-6">
+          <label htmlFor="observationNotes" className="block text-sm font-medium">Observation Notes</label>
+          <textarea
+            id="observationNotes"
+            name="observationNotes"
+            value={formData.observationNotes}
+            onChange={handleChange}
+            rows={4}
+            placeholder='observationNotes'
+            className="mt-1 p-2 w-full border border-gray-300  focus:outline-none focus:ring focus:border-white bg-transparent"
+            required
+          ></textarea>
+        </div>
+
+        {/* Media (Photo or Video) */}
+        <div className="mb-6">
+          <label htmlFor="media" className="block text-sm font-medium">Media (Photo or Video)</label>
+          <input
+            type="file"
+            id="media"
+            name="media"
+            accept="image/*,video/*"
+            onChange={handleChange}
+            className="mt-1 p-2 w-full border border-gray-300  focus:outline-none focus:ring focus:border-white"
+          />
+        </div>
+
+        {/* Submit Button */}
+        <div className="flex items-center justify-end">
+          <button
+            type="submit"
+            className="px-4 py-2 bg-transparent text-white  hover:bg-white hover:text-black border border-white focus:outline-none focus:ring focus:border-white"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default page;
--- app/api/logs/page.tsx
+++ app/api/logs/page.tsx
@@ -3,6 +3,7 @@ import VideoBackground from '@/app/components/VideoBg'
 import '../../../app/globals.css'
 import Logcard from '@/app/components/Logcard'
 import { GiFallingStar } from 'react-icons/gi'
+import Link from 'next/link'
 
 const page = () => {
     return (
@@ -11,7 +12,7 @@ const page = () => {
             <div className=' text-white border border-white mx-[10%] px-2 py-4 mt-[2%] h-full  '>
              <h1 className=' text-5xl bg-white text-black p-2 inline-block'>LOGS!!</h1>
              <div className=' mt-4 overflow-y-scroll py-2 scrollbar'><Logcard/></div>
-             <div className='py-2'><button className=' border border-white p-1 flex  hover:bg-white hover:text-black '>create_a_log <span className='p-1 text-1xl'><GiFallingStar/></span></button></div>
+             <div className='py-2'><button className=' border border-white p-1  hover:bg-white hover:text-black '><Link href='./logs/logform' className='flex'>create_a_log <span className='p-1 text-1xl'><GiFallingStar/></span></Link></button></div>
             </div>
         </div>
     )
