16be8bc838a39f33f7645739a2ed42f386a6a7b4
HARISH-SATHISH <gowdaharish580@gmail.com>
2023-12-13 19:01:49 +0530

    removed auth

--- app/api/auth/[...nextauth]/options.ts
+++
@@ -1,9 +0,0 @@
-import type { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const option:NextAuthOptions={
-    providers:[ GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-      })]
-}
